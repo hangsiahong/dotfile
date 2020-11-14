055437a35391cde8c9fabc801342b67f8dfe0cb0
hangsiahong <honghangsia@gmail.com>
2020-11-14 15:19:18 +0700

    update config

---
+++ fish/config.fish
@@ -0,0 +1,63 @@
+
+
+# Neovim aliases
+alias vi="nvim"
+alias vim="nvim"
+alias v="nvim ."
+
+
+# cargo aliases
+alias cb="cargo buld"
+alias cab="cargo build"
+
+alias cr="cargo run"
+
+# cargo build documentations
+alias bd="cargo doc --open"
+alias rd="cargo docserver"
+
+
+
+alias cat='bat'
+alias ls='exa'
+
+
+# Enter Config fish
+alias configfish='vi ~/.config/fish/config.fish'
+alias configtmux='vi ~/.tmux.conf'
+alias configvim='vi ~/.config/nvim/init.vim'
+alias configvi'vi ~/.config/nvim/init.vim'
+
+# Open Dolphin
+alias o='open . &'
+
+
+alias l='exa --long --header --git --all --sort name'
+alias la='exa -a --long --header --sort name'
+alias md='mkdir -p'
+alias mkdir='mkdir -p'
+alias mosml='rlwrap mosml -P full'
+alias o='open . &'
+alias h='history'
+
+alias path="echo $PATH | tr : '\n'"
+
+# Alias cd
+alias dt='cd ~/Desktop'
+alias ..='cd ..'
+
+# Alias clear and ls
+alias cl='clear; l'
+alias cls='clear; ls'
+alias c='clear'
+alias ca='cargo'
+# Markdown review
+alias marklive='markdown_previewer '
+
+
+# Search
+alias search="rg"
+
+
+# Alias Remove
+alias rm='rm -rf'
