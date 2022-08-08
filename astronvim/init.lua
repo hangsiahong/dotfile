5793e3d90487ada27447d554d7079328b5d978ac
hangsiahong <honghangsia@gmail.com>
2022-08-08 10:12:30 +0700

    add rust

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -43,3 +43,33 @@ local opts = { noremap = true, silent = true }
 
 vim.keymap.set({'n', 'i'}, "<C-+>", function() ResizeGuiFont(1)  end, opts)
 vim.keymap.set({'n', 'i'}, "<C-->", function() ResizeGuiFont(-1) end, opts)
+
+-- LSP RustAnalyzer
+local nvim_lsp = require'lspconfig'
+
+local on_attach = function(client)
+    require'completion'.on_attach(client)
+end
+
+nvim_lsp.rust_analyzer.setup({
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
+
